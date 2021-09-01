e1bbd447f84654406b588ccf6325b4be210797d0
ToddComte <comtetodd@gmail.com>
2021-09-01 10:19:26 -0600

    add datebase

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+first_user = User.where(email: 'snow@monsterapartments.com').first
+apartments = [
+    {
+        street: '101 Blackhawck Streeet',
+        city: 'Payson',
+        state: 'Utah',
+        manager: 'Snow',
+        email: 'snow@monsterapartments.com',
+        price: '2000',
+        bedrooms: 4,
+        bathrooms: 3,
+        pets: 'All are Welcome'
+    },
+    {
+        street: '102 Blackhawck Streeet',
+        city: 'Payson',
+        state: 'Utah',
+        manager: 'Snow',
+        email: 'snow@monsterapartments.com',
+        price: '2000',
+        bedrooms: 4,
+        bathrooms: 3,
+        pets: 'All are Welcome'
+    }
+]
+
+second_user = User.where(email: 'kyle@monsterapartments.com').second
+apartments = [
+    {
+        street: '103 Blackhawck Streeet',
+        city: 'Payson',
+        state: 'Utah',
+        manager: 'Snow',
+        email: 'kyle@monsterapartments.com',
+        price: '2000',
+        bedrooms: 4,
+        bathrooms: 3,
+        pets: 'All are Welcome'  
+    },
+    {
+        street: '104 Blackhawck Streeet',
+        city: 'Payson',
+        state: 'Utah',
+        manager: 'Snow',
+        email: 'kyle@monsterapartments.com',
+        price: '2000',
+        bedrooms: 4,
+        bathrooms: 3,
+        pets: 'All are Welcome'
+    },
+    {
+        street: '105 Blackhawck Streeet',
+        city: 'Payson',
+        state: 'Utah',
+        manager: 'Snow',
+        email: 'kyle@monsterapartments.com',
+        price: '2000',
+        bedrooms: 4,
+        bathrooms: 3,
+        pets: 'All are Welcome'
+    }
+]
+
+User.each do |attributes|
+    user.create attributes
+    puts "creating user #{attributes}"
+  end
